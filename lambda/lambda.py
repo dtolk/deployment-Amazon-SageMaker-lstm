9e703d0e3baea1f13e263345bdf68db19c358cea
Denis <d.tolkunov@gmail.com>
2021-09-15 15:27:56 -0700

    added lambda

---
+++ lambda/lambda.py
@@ -0,0 +1,26 @@
+# This is the code that needs to be executed when the lambda function is triggered
+# Copy and paste the code into the Lambda code editor, replace the **ENDPOINT NAME HERE** 
+# portion with the name of the endpoint that we deployed earlier. 
+
+# We need to use the low-level library to interact with SageMaker since the SageMaker API
+# is not available natively through Lambda.
+import boto3
+
+def lambda_handler(event, context):
+
+    # The SageMaker runtime is what allows us to invoke the endpoint that we've created.
+    runtime = boto3.Session().client('sagemaker-runtime')
+
+    # Now we use the SageMaker runtime to invoke our endpoint, sending the review we were given
+    response = runtime.invoke_endpoint(EndpointName = '**ENDPOINT NAME HERE**',    # The name of the endpoint we created
+                                       ContentType = 'text/plain',                 # The data format that is expected
+                                       Body = event['body'])                       # The actual review
+
+    # The response is an HTTP response whose body contains the result of our inference
+    result = response['Body'].read().decode('utf-8')
+
+    return {
+        'statusCode' : 200,
+        'headers' : { 'Content-Type' : 'text/plain', 'Access-Control-Allow-Origin' : '*' },
+        'body' : result
+    }
